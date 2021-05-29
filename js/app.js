287ef2d548c0f400c1a09dd5878f4cc0e1611bcc
lucas ramirez <lucas01.ramirez@gmail.com>
2021-05-29 16:23:07 -0300

    formulario funcional v2

--- js/app.js
+++ js/app.js
@@ -70,3 +70,14 @@ $(document).ready(function() {
     });
   });
   
+  const $form = document.querySelector('#reservation')
+  const $buttonMailto = document.querySelector('#trucazo')
+
+  $form.addEventListener('submit', handleSubmit)
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const form = new FormData(this)
+    $buttonMailto.setAttribute('href', `mailto:lukitasr8@gmail.com?subject=nombre ${form.get('name')}  correo ${form.get('email')}&body=${form.get('service')}`)
+    $buttonMailto.click()
+  }
